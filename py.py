1ad8af6e93cf816a7c6cc6013c68265beef31d8d
Shashank <shashanksaini67@gmail.com>
2021-11-23 21:39:00 +0530

    Update py.py

--- py.py
+++ py.py
@@ -1,65 +1,52 @@
+
 import random
 from typing import no_type_check
-
-
-def game(comp, you):
-    if comp == you:
-        return None
-    elif comp == 's':
-        if you == 'w':
-            return False
-        elif you == 'g':
-            return True
-    elif comp == 'w':
-        if you == 'g':
-            return False
-        elif you == 's':
-            return True
-    elif comp == 'g':
-        if you == 's':
-            return False
-        elif you == 'w':
-            return True
-
-
 print("computer's turn:snake(s) water(w) or gun(g)?")
-randno = random.randint(1, 3)
-if randno == 1:
-    comp = 's'
-elif randno == 2:
-    comp = 'w'
-elif randno == 3:
-    comp = 'g'
-you = input("your turn:snake(s) water(w) or gun(g)?\n")
+randno=random.randint(1,3)
+if randno==1:
+    comp='s'
+elif randno==2:
+    comp='w'
+elif randno==3:
+    comp='g'
+you=input("your turn:snake(s) water(w) or gun(g)?\n")
+def game(comp,you):
+    if comp==you:
+        print("its a tie")
+        return None
+    elif comp=='s':
+        if you=='w':
+            print("you lost!")
+            return False
+        elif you=='g':
+            print("you won")
+            return True
+    elif comp=='w':
+        if you=='g':
+            print("you lost")
+            return False
+        elif you=='s':
+            print("you won")
+            return True
+    elif comp=='g':
+        if you=='s':
+            print("you lost")
+            return False
+        elif you=='w':
+            print("you won")
+            return True
+print(f"computer chose {comp}")
 
-if you == 's':
+
+if you=='s':
     print(f"your chosen key is {you}")
-elif you == 'w':
+elif you=='w':
     print(f"your chosen key is {you}")
-elif you == 'g':
+elif you=='g':
     print(f"your chosen key is {you}")
 else:
     print('''                                                      your chosen key is not valid
-                                            choose betweet the prefix of snake,water or gun's ''')
+                                            choose betweet the prefix of snake,water or gun's ''' )
 
-a = game(comp, you)
-
-print(f"computer chose {comp}")
-
-if comp == you:
-    print("its a tie because you both chose the same keyword")
-elif comp == 'w' and you == 'g':
-    print(f"comp wins bcs it choses {comp} and you chose {you}")
-elif comp == 'w' and you == 's':
-    print(f"comp loses bcs it choses {comp} and you chose {you}")
-elif comp == 'g' and you == 's':
-    print(f"comp wins bcs it choses {comp} and you chose {you}")
-else:
-    print("NOW IT IS FUN ^.^ KEEP PLAYING")
-
-
-
-
-
-
+a=game(comp,you)
 
